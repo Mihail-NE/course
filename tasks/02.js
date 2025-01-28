4ae983c17cd2f5cc348b039c1ce75fe5318080a3
Mihail-NE <157212562+Mihail-NE@users.noreply.github.com>
2025-01-29 00:59:21 +0300

    add task

---
+++ tasks/02.js
@@ -0,0 +1,53 @@
+function mySlice(arr, start, end) {
+    if (start === undefined) {
+        start = 0;
+    }
+    if (end === undefined) {
+        end = arr.length;
+    }
+    if (start < 0) {
+        start = arr.length + start;
+    }
+    if (end < 0) {
+        end = arr.length + end;
+    }
+    if (start > end) {
+        return [];
+    }
+
+    let result = [];
+    for (let i = start; i < end; i++) {
+        result.push(arr[i]);
+    }
+    return result;
+}
+
+function myIndexOf(arr, item, from) {
+    if (from === undefined) {
+        from = 0;
+    }
+    if (from < 0) {
+        from = arr.length + from;
+    }
+    for (let i = from; i < arr.length; i++) {
+        if (arr[i] === item) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+function myIncludes(arr, item, from) {
+    if (from === undefined) {
+        from = 0;
+    }
+    if (from < 0) {
+        from = arr.length + from;
+    }
+    for (let i = from; i < arr.length; i++) {
+        if (arr[i] === item) {
+            return true;
+        }
+    }
+    return false;
+}
